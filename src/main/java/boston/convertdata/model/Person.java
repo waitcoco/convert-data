3de42b34565a9a799e5f9fe71db91b3fe5c8aa0f
waitcoco <jin.zha@husky.neu.edu>
2018-06-01 11:10:35 +0800

    add person

--- src/main/java/boston/convertdata/model/Person.java
+++ src/main/java/boston/convertdata/model/Person.java
@@ -5,6 +5,10 @@ import lombok.Data;
 @Data
 public class Person {
     private String personId;
+    private String personLeftbottom;
+    private String personLefttop;
+    private String personRightbottom;
+    private String personRighttop;
     private String coatColor;
     private String pantsColor;
     private String bodyType;
--- src/main/java/boston/convertdata/model/PersonTask.java
+++ src/main/java/boston/convertdata/model/PersonTask.java
@@ -4,10 +4,10 @@ import lombok.Data;
 
 @Data
 public class PersonTask {
-    private Person person;
     private VideoInfo videoInfo;
     private CameraInfo cameraInfo;
     private FrameInfo frameInfo;
+    private Person person;
 
     public PersonTask(Person person, VideoInfo videoInfo, CameraInfo camera_info, FrameInfo frame_info){
         this.person = person;
