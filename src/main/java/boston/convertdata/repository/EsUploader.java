bd966377865f42ce85bfd09539db820adccf7479
Kai Yang <kfstorm@dingtalk.com>
2018-07-02 12:50:47 +0800

    Make EsUploader thread safe

--- src/main/java/boston/convertdata/repository/EsUploader.java
+++ src/main/java/boston/convertdata/repository/EsUploader.java
@@ -13,13 +13,11 @@ import java.io.IOException;
 import java.util.*;
 import java.util.logging.Logger;
 
-public class EsUploader implements AutoCloseable {
+public class EsUploader {
     private final RestHighLevelClient esClient;
     private final String indexName;
     private final String type;
     private final int batchSize;
-    private final Map<String, String> queue = new HashMap<>();
-    private int count = 0;
     private static final Logger logger = Logger.getLogger(EsUploader.class.getName());
 
     public EsUploader(RestHighLevelClient restHighLevelClient, String indexName, String type, int batchSize) {
@@ -33,13 +31,6 @@ public class EsUploader implements AutoCloseable {
         val response = esClient.getLowLevelClient().performRequest("HEAD", "/" + indexName);
         int statusCode = response.getStatusLine().getStatusCode();
         return statusCode != 404;
-    }
-
-    public void addJsonDocument(String id, String json) throws IOException {
-        queue.put(id, json);
-        if (queue.size() >= batchSize) {
-            flush();
-        }
     }
 
     public void deleteIndex() throws IOException {
@@ -56,27 +47,43 @@ public class EsUploader implements AutoCloseable {
         logger.info("Created index: " + indexName);
     }
 
-    public void flush() throws IOException {
-        if (!queue.isEmpty()) {
-            val bulkRequest = new BulkRequest();
-            queue.forEach((id, json) -> {
-                val indexRequest = new IndexRequest(indexName, type, id).source(json, XContentType.JSON);
-                bulkRequest.add(indexRequest);
-            });
-            BulkResponse bulkResponse;
-            bulkResponse = esClient.bulk(bulkRequest);
-            if (bulkResponse.hasFailures()) {
-                throw new RuntimeException(bulkResponse.buildFailureMessage());
-            }
-
-            count += queue.size();
-            queue.clear();
-            logger.info("Imported " + count + " documents.");
-        }
+    public Session newSession() {
+        return new Session();
     }
 
-    @Override
-    public void close() throws Exception {
-        esClient.close();
+    public class Session implements AutoCloseable {
+        private final Map<String, String> queue = new HashMap<>();
+        private int count = 0;
+
+        public void addJsonDocument(String id, String json) throws IOException {
+            queue.put(id, json);
+            if (queue.size() >= batchSize) {
+                flush();
+            }
+        }
+
+        public void flush() throws IOException {
+            if (!queue.isEmpty()) {
+                val bulkRequest = new BulkRequest();
+                queue.forEach((id, json) -> {
+                    val indexRequest = new IndexRequest(indexName, type, id).source(json, XContentType.JSON);
+                    bulkRequest.add(indexRequest);
+                });
+                BulkResponse bulkResponse;
+                bulkResponse = esClient.bulk(bulkRequest);
+                if (bulkResponse.hasFailures()) {
+                    throw new RuntimeException(bulkResponse.buildFailureMessage());
+                }
+
+                count += queue.size();
+                queue.clear();
+                logger.info("Imported " + count + " documents.");
+            }
+        }
+
+        @Override
+        public void close() throws Exception {
+            flush();
+        }
     }
 }
